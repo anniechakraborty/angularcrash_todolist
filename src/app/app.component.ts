938317489dff7efe5ee88694575a346cb0434a26
anniechakraborty <the.demented.genius29@gmail.com>
2023-01-05 00:00:22 +0530

    testing angular basics and also added global basic styles from the video tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularcrash_todolist';
+  title : string = 'Task Tracker';
 }
